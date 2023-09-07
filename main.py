390fdaa370040e00bca857fdddbeb5d4b01b120f
unknown <mdphuc1110@gmail.com>
2023-09-07 17:29:25 -0400

    v2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_file
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
@@ -8,6 +8,15 @@ def main():
 @app.route("/project/<ProjectName>", methods=['GET'])
 def project(ProjectName):
     return render_template(f"{ProjectName}.html")
+
+@app.route("/project/<BlogName>", methods=['GET'])
+def blog(BlogName):
+    return render_template(f"{BlogName}.html")
+
+@app.route('/resume')
+def resume():
+    path = './static/file/PhucMai_resume.pdf'
+    return send_file(path, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5500, debug=True)
