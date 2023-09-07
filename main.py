9cf18f59d083437005c4a83db2abbc14420bc5d0
unknown <mdphuc1110@gmail.com>
2023-09-07 09:52:22 -0400

    v1.0

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def main():
     return render_template("index.html")
 
+@app.route("/project/<ProjectName>", methods=['GET'])
+def project(ProjectName):
+    return render_template(f"{ProjectName}.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5500, debug=True)
+
